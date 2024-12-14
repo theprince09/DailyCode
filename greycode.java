cb073c17cb759c8b8c7b2c6bc2e9af8743678798
Prince Raj <114799883+heyyprince09@users.noreply.github.com>
2024-12-14 08:36:56 +0530

    Add files via upload

---
+++ greycode.java
@@ -0,0 +1,61 @@
+/*
+ Grey Code Converter
+
+ An n-bit gray code sequence is a sequence of 2n integers where:
+
+Every integer is in the inclusive range [0, 2n - 1],
+The first integer is 0,
+An integer appears no more than once in the sequence,
+The binary representation of every pair of adjacent integers differs by exactly one bit, and
+The binary representation of the first and last integers differs by exactly one bit.
+Given an integer n, return any valid n-bit gray code sequence.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,3,2]
+Explanation:
+The binary representation of [0,1,3,2] is [00,01,11,10].
+- 00 and 01 differ by one bit
+- 01 and 11 differ by one bit
+- 11 and 10 differ by one bit
+- 10 and 00 differ by one bit
+[0,2,3,1] is also a valid gray code sequence, whose binary representation is [00,10,11,01].
+- 00 and 10 differ by one bit
+- 10 and 11 differ by one bit
+- 11 and 01 differ by one bit
+- 01 and 00 differ by one bit
+Example 2:
+
+Input: n = 1
+Output: [0,1]
+
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+       List<Integer> result = new ArrayList<>();
+       int totalNumbers = 1 << n; 
+       for (int i = 0; i < totalNumbers; i++) {
+            result.add(i ^ (i >> 1)); 
+        }
+        return result;
+    }
+}
+
+public class greycode {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int input = sc.nextInt();
+        List<Integer> Code = sol.grayCode(input);
+        System.out.println("Grey Code of " + input+ " is: "+ Code); 
+    }
+}
